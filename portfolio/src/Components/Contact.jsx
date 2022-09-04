1dd293681cabcb861741d048b5eea23ad02836c5
ali_2619 <ali35004879@gmail.com>
2022-09-04 16:44:52 +0300

    Second Commit

--- portfolio/src/Components/Contact.jsx
+++ portfolio/src/Components/Contact.jsx
@@ -2,7 +2,50 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div></div>
+    <div name='contact' className=' h-screen  w-full bg-gradient-to-b from-gray-800 to-black pt-8 text-white'>
+
+      <div className='max-w-screen-lg mx-auto  px-8 flex flex-col justify-center h-full'>
+        <div>
+          <p className='text-white text-bold text-3xl  border-b-4 inline py-2'>Contact</p>
+          <p className='text-[#8892b0] py-6 '>
+          Submit the form below to get in touch with me 
+          </p>
+        </div>
+
+        <div className='flex justify-center items-center'>
+          <form action="" className='flex flex-col w-full md:w-1/2'>
+            <input 
+             type="text"
+             name="name" 
+             id="Enter you name" 
+             className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+             />
+
+            <input 
+             type="text"
+             name="email" 
+             id="Enter your Email" 
+             className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+             />
+              
+              <textarea name="messege" rows="10" className='p-2 bg-transparent rounded-md text-white border-2 focus:outline-none'>
+
+              </textarea>
+
+              <button className='text-white hover:scale-110 duration-300 cursor-pointer  px-6 py-3  my-8
+              mx-auto flex items-center bg-gradient-to-r from-indigo-400 to-indigo-900
+                    border-2 rounded-md'>
+                lets Talk
+
+
+
+              </button>
+          </form>
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
--- portfolio/src/Components/Experience.jsx
+++ portfolio/src/Components/Experience.jsx
@@ -10,22 +10,94 @@ import node from '../assets/node.png'
 import git from '../assets/github.png'
 
 
-
-
-const Experience = () => {
-  return (
-    <div className=''>
-
-      <img src={HTML} alt="icon" />
+{/* 
       <img src={CSS} alt="icon" />
       <img src={Javascript} alt="icon" />
       <img src={react} alt="icon" />
       <img src={tailwind} alt="icon" />
       <img src={aws} alt="icon" />
       <img src={node} alt="icon" />
-      <img src={git} alt="icon" />
+      <img src={git} alt="icon" /> */}
 
-      
+const Experience = () => {
+
+  const iconx = [
+
+          {
+
+           id : 1,
+           src : HTML,
+           title : 'HTML',
+           style : 'shadow-orange-500' 
+          },
+          {
+
+            id : 2,
+            src : CSS,
+            title : 'CSS',
+            style : 'shadow-blue-500' 
+           },
+           {
+
+            id : 3,
+            src : Javascript,
+            title : 'JavaScript',
+            style : 'shadow-yellow-500' 
+           },
+           {
+
+            id : 4,
+            src : react,
+            title : 'React',
+            style : 'shadow-blue-600' 
+           },
+           {
+
+            id : 5,
+            src : tailwind,
+            title : 'Tailwind',
+            style : 'shadow-sky-500' 
+           },
+           {
+
+            id : 6,
+            src : git,
+            title : 'GitHub',
+            style : 'shadow-gray-400' 
+           }
+  ]
+  return (
+    <div name="experience" className=' bg-gradient-to-b from-black to-gray-800 w-full h-screen'>
+        <div className= 'max-w-[1000px] mx-auto  px-8 flex flex-col justify-center h-full '>
+          
+          <div>
+            <p className='text-white text-bold text-3xl  border-b-4 inline py-2'>Experience</p>
+            <p className='text-[#8892b0] py-6 '>These are the technologies I've worked with</p>
+          </div>
+
+          <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+
+
+            {iconx.map(({id,src,title,style})=>
+            
+            (
+              <div key={id} 
+               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+
+              <img src={src} alt="icon"  className='w-20 mx-auto'/>
+              <p className='mt-4 text-white'>{title}</p>
+            </div>
+
+            )
+            
+            )}
+
+            
+
+       
+          </div>
+
+        </div>
     </div>
   )
 }
